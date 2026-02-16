e9e64691c0e195c821e2a4f8fcab3f744486ad6b
Dabjam <lazerfix1@gmail.com>
2026-02-16 22:45:12 +0300

    completed pracrice_2

--- practice_2/app_test.js
+++ practice_2/app_test.js
@@ -1,57 +1,68 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-let users = [
-{id: 1, name: 'Петр'
-, age: 16},
-{id: 2, name: 'Иван'
-, age: 18},
-{id: 3, name: 'Дарья'
-, age: 20},
+const PORT = 3000;
+
+let products = [
+    {id: 3, name: "Банан", price: 20},
+    {id: 4, name: "Апельсин", price: 21},
+    {id: 5, name: "Яблоко", price: 22},
 ]
 
-// Middleware для парсинга JSON
 app.use(express.json());
-// Главная страница
-app.get('/'
-, (req, res) => {
-res.send('Главная страница');
+
+app.get('/', (req, res) => {
+   res.send("Главная страница")
 });
-// CRUD
-app.post('/users'
-, (req, res) => {
-const { name, age } = req.body;
-const newUser = {
-id: Date.now(),
-name,
-age
-};
-users.push(newUser);
-res.status(201).json(newUser);
-});
-app.get('/users'
-, (req, res) => {
-res.send(JSON.stringify(users));
-});
-app.get('/users/:id'
-, (req, res) => {
-let user = users.find(u => u.id == req.params.id);
-res.send(JSON.stringify(user));
-});
-app.patch('/users/:id'
-, (req, res) => {
-    const user = users.find(u => u.id == req.params.id);
-const { name, age } = req.body;
-if (name !== undefined) user.name = name;
-if (age !== undefined) user.age = age;
-res.json(user);
-});
-app.delete('/users/:id'
-, (req, res) => {
-users = users.filter(u => u.id != req.params.id);
-res.send('Ok');
-});
-// Запуск сервера
-app.listen(port, () => {
-console.log(`Сервер запущен на http://localhost:${port}`);
-});
+
+app.get('/products', (req, res) => {
+    res.json(products);
+})
+
+app.post('/products', (req, res) => {
+    if(!req.body || !req.body.name) {
+        return res.status(400).json({error: "Некорректный JSON или пустые данные"})
+    }
+    const newProduct = {
+        id: products.length > 0 ? products.at(-1).id + 1 : 1,
+        name: req.body.name,
+        price: req.body.price
+    };
+
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+})
+
+
+app.get('/products/:id', (req, res) => {
+    let product = products.find( x => x.id == req.params.id);
+    if (product) {
+        res.json(product)
+    }
+    else {
+        res.status(404).json({error: "Продукт не найден"});
+    }
+    
+})
+
+app.patch('/products/:id', (req, res) => {
+    let product = products.find( x => x.id == req.params.id);
+    const {name, price} = req.body;
+
+    if (!product) {
+        return res.status(404).json({error: "Товар не найден"});
+    }
+
+    if (name != undefined) product.name = name;
+    if (price != undefined) product.price = price;
+    
+    res.json(product);
+})
+
+app.delete('/products/:id', (req, res) => { 
+    products = products.filter(x => x.id != req.params.id);
+    res.status(200).json({ message: "Товар удален" });
+})
+
+app.listen(PORT, () => {
+    console.log(`((200) Сервер запущен: http://localhost:${PORT}`)
+})
